08ae5799b16b587a6e920174895abfe0dab16a19
Kevin <me@bradtravery.com>
2019-11-17 15:14:31 -0500

    html done

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
-import 'bootstrap';
-import $ from 'jquery';
+import "bootstrap";
+import $ from "jquery";
 
-import './index.scss';
+import "./index.scss";
 
 $(document).ready(function() {
   //header button
-  $('.closeHeaderButton').click(function() {
-    $('#intro').slideUp(1000);
+  $(".closeHeaderButton").click(function() {
+    $("#intro").animate({ bottom: "100vh" });
+    $("main").show();
+  });
+
+  //navigation links
+  $(".nav-link").click(function() {
+    let page = $(this).attr("page");
+    $("main section").hide();
+    $("#" + page).show();
+    $("#" + page)
+      .nextAll()
+      .show();
+  });
+
+  //next page button
+  $(".next-page-button").click(function() {
+    let page = $(this).attr("page");
+    let nextPage = $(this).attr("next-page");
+    $("#" + page).hide();
+    $("#" + nextPage).show();
   });
 });
