4de3d67285de3d4ff166f5b2407da541253fe2c3
Kevin Gao <37745638+KevinBoxuGao@users.noreply.github.com>
2019-11-28 15:05:00 -0500

    device transfer

--- src/app/generation/circuitMagnet/CircuitMagnet.js
+++ src/app/generation/circuitMagnet/CircuitMagnet.js
@@ -2,39 +2,31 @@ import React, { Component } from "react";
 import $ from "jquery";
 import "./CircuitMagnet.scss";
 
-import Circuit from "../../../../assets/Circuit.svg";
+import Circuit from "../../../../assets/Rectangle 1.svg";
 import Magnet from "../../../../assets/Magnet.svg";
 import Electron from "../../../../assets/Electron-Yellow.svg";
 
 class CircuitMagnet extends Component {
   handleClick = () => {
-    $(".electron").animate(
-      {
-        left: "+=40%"
-      },
-      1000,
-      function() {
-        $(".electron").animate({ left: "-=40%" }, 1000);
-      }
-    );
+    $(".magnet").animate({left: "+=60%"}, 1500);
+    $(".electron").animate({left: "+=60%"}, 1500);
+    $(".magnet").animate({left: "-=60%"}, 1500);
+    $(".electron").animate({ left: "-=60%" }, 1500);
   };
-
   render() {
     return (
       <div className="circuit-magnet container-fluid">
         <div className="magnet-row row justify-content-center">
-          <img className="magnet" src={Magnet} />
         </div>
         <div className="row justify-content-center">
-          <div className="col col-md-9">
-            <div>
-              <img className="circuit" src={Circuit} />
-              <img className="electron" src={Electron} />
-            </div>
+          <div className="col col-md-9 diagram-container d-flex">
+            <img className="magnet" src={Magnet} />
+            <img className="circuit" src={Circuit} />
+            <img className="electron" src={Electron} />
           </div>
         </div>
-        <div className="row">
-          <button onClick={() => this.handleClick()}>yes</button>
+        <div className="row justify-content-center">
+          <button className="button" onClick={() => this.handleClick()}>Play Animation</button>
         </div>
       </div>
     );
